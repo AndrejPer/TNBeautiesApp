6197ee0e89043a0d37ef7b34d88b355cda0194a5
AndrejPer <andrej.perkovic@icloud.com>
2022-08-12 21:58:19 +0200

    welcome screen without validation

---
+++ tnbeautiesapp/lib/signupform.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:tnbeautiesapp/passwordfields.dart';
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _dateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'First name',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your last name';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Last name',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your last name';
+              }
+              return null;
+            },
+          ),
+          TextField(
+            readOnly: true,
+            controller: _dateController,
+            decoration: const InputDecoration(
+              hintText: 'Date of birth',
+            ),
+            onTap: () async {
+              var date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1900),
+                lastDate: DateTime.now(),
+              );
+              _dateController.text = date.toString().substring(0, 10);
+            },
+          ),
+          PasswordField(),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text('Processing')));
+              }
+            },
+            child: const Text('Confirm'),
+          )
+        ],
+      ),
+    );
+  }
+}
