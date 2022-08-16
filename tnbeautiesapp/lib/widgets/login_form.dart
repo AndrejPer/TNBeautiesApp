4b93dfbedac6d819d2094920876c88c38f45b2f3
AndrejPer <andrej.perkovic@icloud.com>
2022-08-16 22:54:18 +0200

    login and signup connected to database

--- tnbeautiesapp/lib/widgets/login_form.dart
+++ tnbeautiesapp/lib/widgets/login_form.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/user.dart';
+import '../screens/home_page.dart';
 
 class LoginForm extends StatefulWidget {
   const LoginForm({Key? key}) : super(key: key);
@@ -18,69 +23,72 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: null,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                hintText: 'Enter email',
+    return SingleChildScrollView(
+      child: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter email',
+                ),
+                validator: (value) {
+                  RegExp emailExp = RegExp(
+                      r'^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$');
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email adress';
+                  } else if (!emailExp.hasMatch(value)) {
+                    return 'Please enter a valid adress';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                RegExp emailExp = RegExp(
-                    r'^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$');
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your email adress';
-                } else if (!emailExp.hasMatch(value)) {
-                  return 'Please enter a valid adress';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                hintText: 'Enter password',
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Enter password',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                },
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a password';
-                }
-              },
-            )
-          ],
+              ElevatedButton(onPressed: null, child: const Text('Log in'))
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Future userLogin() async {
-    //String email = _emailController.text;
-    //String password = _passwordController.text;
+  Future<bool> userLogin() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
 
-    String email = 'andrej.email.com';
-    String password = 'P4ssword?';
-
-    var data = {'email': email, 'password': password};
+    var data = {'email': "andre@email.com", 'password': "P4ssword?"};
+    print('gonna try');
     var url = Uri(
       scheme: 'https',
       host: 'student.famnit.upr.si',
-      path: '/~89201045/login.php',
+      path: '/~89201045/get.php',
     );
 
-    return http.post(url, body: data).then((http.Response response) {
-      final int statusCode = response.statusCode;
+    var response = await http.post(url, body: data);
+    var result = jsonDecode(response.body);
 
-      if (statusCode < 200 || statusCode > 400 || json == null) {
-        throw Exception("Error while fetching data");
-      }
-      print('code good');
-      print(response.body);
-      return User.fromJson(json.decode(response.body));
-    });
+    print('result is: $result');
+
+    if (result != "Error") {
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+      await preferences.setString("userJson", response.body);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   Future getData() async {
@@ -88,11 +96,13 @@ class _LoginFormState extends State<LoginForm> {
       scheme: 'https',
       host: 'student.famnit.upr.si',
       path: '/~89201045/login.php',
+      port: 22,
     );
     // 'https://student.famnit.upr.si/~89201045/get.php';
+    print('gonna check $url');
 
     http.Response response = await http.get(url);
-    print(response.statusCode);
+    print('response code is ${response.statusCode}');
 
     var user = User.fromJson(jsonDecode(response.body));
     print(user);
@@ -102,7 +112,7 @@ class _LoginFormState extends State<LoginForm> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    //getData();
-    Future user = userLogin();
+    getData();
+    //Future<bool> flag = userLogin();
   }
 }
--- tnbeautiesapp/lib/widgets/sign_up_form.dart
+++ tnbeautiesapp/lib/widgets/sign_up_form.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:tnbeautiesapp/widgets/email_field.dart';
-import 'package:tnbeautiesapp/widgets/password_field.dart';
+import 'package:http/http.dart' as http;
 
 class SignUpForm extends StatefulWidget {
   const SignUpForm({Key? key}) : super(key: key);
@@ -11,69 +12,148 @@ class SignUpForm extends StatefulWidget {
 
 class _SignUpFormState extends State<SignUpForm> {
   final _formKey = GlobalKey<FormState>();
-  final _dateController = TextEditingController();
+  final fnameCntroller = TextEditingController();
+  final lnameCntroller = TextEditingController();
+  final emailCntroller = TextEditingController();
+  final dateController = TextEditingController();
+  final passwordCntroller = TextEditingController();
+  bool _isObscure = true;
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'First name',
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: fnameCntroller,
+                decoration: const InputDecoration(
+                  hintText: 'First name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your last name';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your last name';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'Last name',
+              TextFormField(
+                controller: lnameCntroller,
+                decoration: const InputDecoration(
+                  hintText: 'Last name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your last name';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your last name';
-                }
-                return null;
-              },
-            ),
-            const EmailField(),
-            TextField(
-              readOnly: true,
-              controller: _dateController,
-              decoration: const InputDecoration(
-                hintText: 'Date of birth',
+              TextFormField(
+                controller: emailCntroller,
+                decoration: const InputDecoration(
+                  hintText: 'Email address',
+                ),
+                validator: (value) {
+                  RegExp emailExp = RegExp(
+                      r'^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$');
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email adress';
+                  } else if (!emailExp.hasMatch(value)) {
+                    return 'Please enter a valid adress';
+                  }
+                  return null;
+                },
               ),
-              onTap: () async {
-                var date = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(1900),
-                  lastDate: DateTime.now(),
-                );
-                _dateController.text = date.toString().substring(0, 10);
-              },
-            ),
-            const PasswordField(),
-            ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing')));
-                }
-              },
-              child: const Text('Confirm'),
-            )
-          ],
+              TextField(
+                readOnly: true,
+                controller: dateController,
+                decoration: const InputDecoration(
+                  hintText: 'Date of birth',
+                ),
+                onTap: () async {
+                  var date = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime.now(),
+                  );
+                  dateController.text = date.toString().substring(0, 10);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Choose a password',
+                  // this button is used to toggle the password visibility
+                  suffixIcon: IconButton(
+                      icon: Icon(
+                          _isObscure ? Icons.visibility : Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      }),
+                ),
+                obscureText: _isObscure,
+                validator: (value) {
+                  RegExp regExp = RegExp(
+                      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$');
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  } else if (!regExp.hasMatch(value)) {
+                    return 'Enter a valid password (Capital letter, number, symbole)';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing')));
+                  } else {
+                    registerUser();
+                  }
+                },
+                child: const Text('Confirm'),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future registerUser() async {
+    var url = Uri(
+      scheme: 'https',
+      host: 'student.famnit.upr.si',
+      path: '/~89201045/register.php',
+      port: 22,
+    );
+
+    var data = {
+      'first_name': fnameCntroller.text,
+      'last_name': lnameCntroller.text,
+      'email': emailCntroller.text,
+      'birthday': dateController.text,
+      'password': passwordCntroller,
+    };
+
+    http.Response response = await http.post(url, body: data);
+
+    if (jsonDecode(response.body) == "Error") {
+      const AlertDialog(
+        title: Text('Oups'),
+      );
+    } else {
+      const AlertDialog(
+        title: Text('Registered'),
+      );
+    }
   }
 }
